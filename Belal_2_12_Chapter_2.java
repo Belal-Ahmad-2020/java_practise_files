556cfb4cf059ed85b692eda91ae71afccc93d048
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-17 23:49:41 +0430

    Add files via upload

---
+++ Belal_2_12_Chapter_2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_12_Chapter_2 {
+    public static void main(String[] args) {
+//        Given that y = ax3 + 7, which of the following are correct Java statements for this equation?
+    int y = 0, a = 0, x = 0;
+    y = a * (x * x * x) + 7;
+    
+//    e) y = a * (x * x * x) + 7;  is correct
+
+
+
+
+    }
+}
