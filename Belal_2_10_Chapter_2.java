0076459386878ddfeb991ef3295db9bb902f5839
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-17 23:48:40 +0430

    Add files via upload

---
+++ Belal_2_10_Chapter_2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_10_Chapter_2 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+//        a) System.out.printf("x = %d%n", x);   --> x = 2
+             System.out.printf("x = %d%n", x);
+             
+//  b) System.out.printf("Value of %d + %d is %d%n", x, x, (x + x));    --> 4           
+        System.out.printf("Value of %d + %d is %d%n", x, x, (x + x));
+        
+// c) System.out.printf("x =");  --> x =
+     System.out.printf("x =");
+        
+//     d) System.out.printf("%d = %d%n", (x + y), (y + x));  --> x = 5 = 5
+         System.out.printf("%d = %d%n ", (x + y), (y + x));
+         
+         
+    }
+}
