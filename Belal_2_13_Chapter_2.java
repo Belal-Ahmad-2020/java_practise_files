444813d24e0aca3a5d7a38e3886bccdd680ba069
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-17 23:50:18 +0430

    Add files via upload

---
+++ Belal_2_13_Chapter_2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_13_Chapter_2 {
+    public static void main(String[] args) {
+//        show the value of x after each statement is performed:
+
+//  a) x = 7 + 3 * 6 / 2 - 1;
+    int x = 7 + 3 * 6 / 2 - 1;  // -- 15
+        System.out.println(x);
+        
+        
+//   b) x = 2 % 2 + 2 * 2 - 2 / 2;
+     x = 2 % 2 + 2 * 2 - 2 / 2;    // -- 3
+        System.out.println(x);
+        
+        
+//   c) x = (3 * 9 * (3 + (9 * 3 / (3))));
+        x = (3 * 9 * (3 + (9 * 3 / (3))));  // 324         
+        System.out.println(x);
+        
+    }
+}
