277ea9bb8f7e609876d6fc20a76de2f6677a012d
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-17 23:49:08 +0430

    Add files via upload

---
+++ Belal_2_11_Chapter_2.java
@@ -0,0 +1,37 @@
+package auaf;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_11_Chapter_2 {
+    public static void main(String[] args) {
+//  Which of the following Java statements contain variables whose values are modified?
+
+//  a) p = i + j + k + 7;
+/*
+    p, i, j, k are variables and the value of p is modified 
+*/
+
+// b) System.out.println("variables whose values are modified");
+ System.out.println("variables whose values are modified"); // p
+
+ 
+// c) System.out.println("a = 5");
+     System.out.println("a = 5"); // a = 5
+     
+// d) value = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int  value = input.nextInt();
+        // by this statement we can get an integer value from user
+        
+         
+
+    }
+}
