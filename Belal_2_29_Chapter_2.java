45da44e8ecc2c337d171f7420c75fa32f8f8a92f
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-19 09:56:09 +0430

    Add files via upload

---
+++ Belal_2_29_Chapter_2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+import java.util.Scanner;
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_29_Chapter_2 {
+    public static void main(String[] args) {
+     // (Integer Value of a Character   
+     
+     char character;
+     
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your character: ");
+        character = (in.next().charAt(0));          
+        System.out.println((int)character);
+        
+     
+    }
+
+}
