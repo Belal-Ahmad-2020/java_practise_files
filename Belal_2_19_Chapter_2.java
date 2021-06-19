b6a9764b57a624314a0608216e9b44f2eadbd7ba
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-19 09:48:42 +0430

    Add files via upload

---
+++ Belal_2_19_Chapter_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_19_Chapter_2 {
+    public static void main(String[] args) {
+        // it will display asteriks and lines
+        System.out.printf("*%n**%n***%n****%n*****%n");
+    }
+}
