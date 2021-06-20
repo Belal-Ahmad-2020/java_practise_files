2360176d7dc5ad7a842e780d8ca7c88e1493eeb7
Ahmad Belal Mansoori <67713019+Belal-Ahmad-2020@users.noreply.github.com>
2021-06-20 23:34:17 +0430

    Add files via upload

---
+++ Belal_2_34_Chapter_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auaf;
+
+/**
+ *
+ * @author Full-Stack
+ */
+public class Belal_2_34_Chapter_2 {
+    public static void main(String[] args) {
+//        (World Population Growth Calculator)
+    
+    }
+}
